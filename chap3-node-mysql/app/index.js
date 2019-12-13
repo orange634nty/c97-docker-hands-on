0f90b513679e0fd12ab329ec4c69707e18f99b1f
orange634nty <orange634nty@gmail.com>
2019-12-14 00:41:18 +0900

    より最小構成になるように修正

--- chap3-node-mysql/app/index.js
+++ chap3-node-mysql/app/index.js
@@ -1,21 +1,21 @@
-var express = require('express');
-var app = express();
-var mysql = require('mysql');
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host     : 'mysql',
   user     : 'exapp_user',
   password : 'exapp',
   database : 'exapp_db'
 });
 
-app.get('/', function (req, res) {
-  connection.query('select * from exapp_table', function (error, results, fields) {
+app.get('/', (req, res) => {
+  connection.query('select * from exapp_table', (error, results, fields) => {
     if (error) throw error;
     res.send(results);
   });
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
